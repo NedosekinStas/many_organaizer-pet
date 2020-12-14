245aae3bdad62f1693da9deabe909218946e5a4f
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-12-14 22:24:08 +0300

    add page create routing(lazy loading)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    meta: { layout: 'main' },
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
@@ -17,10 +17,40 @@ const routes = [
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'Register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'Categories',
     meta: { layout: 'main' },
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: { layout: 'main' },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planing',
+    name: 'Planing',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planing.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
   }
 ]
 
