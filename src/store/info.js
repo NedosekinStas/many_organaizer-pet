47ec23ac717501d2fe1155979e6852b0a7ceac1a
NedosekinStas <nedosekinstanislav@gmail.com>
2021-01-25 16:12:12 +0300

    store info
    add actions info, currency

---
+++ src/store/info.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'
+
+export default {
+  state: {
+    info: {}
+  },
+  mutations: {
+    setInfo (state, info) {
+      state.info = info
+    },
+    // Для того что бы фронт не кешировал что либо в state когда делается logout, что бы разные пользователим не пересекались
+    clearInfo (state) {
+      state.info = {}
+    }
+  },
+  actions: {
+    async  fetchInfo ({ dispatch, commit }) {
+      try {
+        const uid = await dispatch('getUid')
+        const info = (await firebase.database().ref(`/users/${uid}/info`).once('value')).val()
+        commit('setInfo', info)
+      } catch (e) {}
+    }
+  },
+  getters: {
+    info: s => s.info
+  }
+}
