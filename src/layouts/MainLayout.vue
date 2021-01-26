5e69c707b59aa38ff01e8f65a81c41db6691c94f
NedosekinStas <nedosekinstanislav@gmail.com>
2021-01-26 23:12:21 +0300

    style guide
    fix errors style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="app-main-layout">
-    <Navbar @clickNav="isOpen = !isOpen" />
-    <Sidebar :isOpen="isOpen" />
-      <div class="app-main-layout">
-        <main class="app-content" :class="{full: !isOpen}">
-        <div class="app-page">
-          <router-view />
-        </div>
-      </main>
+  <div>
+    <Loader v-if="loading"/>
+    <div class="app-main-layout" v-else>
+      <Navbar @clickNav="isOpen = !isOpen" />
+      <Sidebar :isOpen="isOpen" />
+        <div class="app-main-layout">
+          <main class="app-content" :class="{full: !isOpen}">
+          <div class="app-page">
+            <router-view />
+          </div>
+        </main>
 
-      <div class="fixed-action-btn">
-        <router-link class="btn-floating btn-large red" to="/record">
-          <i class="large material-icons">add</i>
-        </router-link>
+        <div class="fixed-action-btn">
+          <router-link class="btn-floating btn-large red" to="/record">
+            <i class="large material-icons">add</i>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -26,17 +29,21 @@ export default {
   name: 'Mainlayout',
   data () {
     return {
-      isOpen: true
+      isOpen: true,
+      loading: true
     }
   },
   components: {
     Navbar,
     Sidebar
   },
+  // Ждем инфу (Если есть то диспачим в компонент)
   async mounted () {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+    // Если есть данные то сделаем false и не будет асинхронной операции
+    this.loading = false
   }
 }
 </script>
